e32e5f98cb959dcc434ca7bed8a13766bdd8a9eb
Chaitrali <chaitralisathe2008@gmail.com>
2026-08-15 12:37:56 -1100

    Fix Render port

--- css/js/app.py
+++ css/js/app.py
@@ -1571,7 +1571,11 @@ if __name__ == "__main__":
     print("====================================")
     print("🚑 RAKSHAK SERVER STARTED")
     print("====================================")
-    print("🌐 http://127.0.0.1:5000")
+
+    import os
+    port = int(os.environ.get("PORT", 5000))
+
+    print(f"🌐 Running on port {port}")
     print("====================================")
 
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=port)
